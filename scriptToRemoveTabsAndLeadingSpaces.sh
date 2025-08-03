c1c742aa697b0df559fe65d1a79a387d24907d7d
Rávilon <ravilon.ads@inf.ufpel.edu.br>
2025-08-03 15:57:08 -0300

    Update scriptToRemoveTabsAndLeadingSpaces.sh

--- scriptToRemoveTabsAndLeadingSpaces.sh
+++ scriptToRemoveTabsAndLeadingSpaces.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed -i -E 's/^[ \t]+//' Jul2026/RepC/*/*.*
 sed -i -E 's/^[ \t]+//' Jul2026/RepCPP/*/*.*
-sed -i -E ':a; /^[[:space:]]*#pragma.*\\$/ { N; s/\\\n[[:space:]]*/ /; ba; }' Jul2026/RepC/*/*.*
-sed -i -E ':a; /^[[:space:]]*#pragma.*\\$/ { N; s/\\\n[[:space:]]*/ /; ba; }' Jul2026/RepCPP/*/*.*
-grep -n '\\$' Jul2026/RepC/*/*.*
-grep -n '\\$' Jul2026/RepCPP/*/*.*
+#sed -i -E ':a; /^[[:space:]]*#pragma.*\\$/ { N; s/\\\n[[:space:]]*/ /; ba; }' Jul2026/RepC/*/*.*
+#sed -i -E ':a; /^[[:space:]]*#pragma.*\\$/ { N; s/\\\n[[:space:]]*/ /; ba; }' Jul2026/RepCPP/*/*.*
+grep '\\$' Jul2026/RepC/*/*.*
+grep '\\$' Jul2026/RepCPP/*/*.*
